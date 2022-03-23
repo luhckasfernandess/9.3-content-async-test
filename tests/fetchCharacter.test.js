ad39659fcea4da477ddb0a81913906b1af48fc5b
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-03-23 16:27:12 -0400

    Tests that checks if it returns an error when executing the function without parameter

--- tests/fetchCharacter.test.js
+++ tests/fetchCharacter.test.js
@@ -12,6 +12,16 @@ describe('Teste a função fetchCharacter', () => {
       // Verifica se não é um falso positivo alterando o resultado esperado para 'Spider Man'
       // expect(character.name).toBe('Spider Man');
 
+      // Foi usado o .toBe pq o retorno é name e name é uma string
       expect(character.name).toBe('Wonder Woman');
+    });
+    
+    it('Checks if it returns an error when executing the function without parameter', async () => {
+      const failRequest = await fetchCharacter();
+      // Observando o retorno da função pelo console.log()
+      // console.log(failRequest);
+
+      // Foi usado .toEqual pq o retorno é new Error, que é um construtor do JavaScript responsável por criar um OBJETO de erro.
+      expect(failRequest).toEqual(new Error('You must provide an url'));
     });
 });
