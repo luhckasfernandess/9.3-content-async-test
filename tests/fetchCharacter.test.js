69dfa33eb30b4c8caaf85704f4f0bd54918242f6
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-03-23 15:13:47 -0400

    Tests that checks if the character's name is 'Wonder Woman'

--- tests/fetchCharacter.test.js
+++ tests/fetchCharacter.test.js
@@ -2,5 +2,16 @@ require('../mocks/fetchSimulator');
 const { fetchCharacter } = require('../src/fetchCharacter');
 
 describe('Teste a função fetchCharacter', () => {
-  it('', () => {});
+    it('Checks if the characters name is Wonder Woman', async () => {
+      // console.log(await fetchCharacter('720'));
+      const character = await fetchCharacter('720');
+
+      // Verificamos se a resposta da API é um objeto e acessamos a chave nome desse objeto para testar o nome da personagem
+      // console.log(character.name);
+
+      // Verifica se não é um falso positivo alterando o resultado esperado para 'Spider Man'
+      // expect(character.name).toBe('Spider Man');
+
+      expect(character.name).toBe('Wonder Woman');
+    });
 });
