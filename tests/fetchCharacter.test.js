ec7aa0e7856fc5d58397ab2bc4eba4362648331d
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-03-23 18:01:03 -0400

    Check if fetch is called with the correct endpoint

--- tests/fetchCharacter.test.js
+++ tests/fetchCharacter.test.js
@@ -24,4 +24,21 @@ describe('Teste a função fetchCharacter', () => {
       // Foi usado .toEqual pq o retorno é new Error, que é um construtor do JavaScript responsável por criar um OBJETO de erro.
       expect(failRequest).toEqual(new Error('You must provide an url'));
     });
+
+    it('Checks if it returns "Invalid id" when executing the function with parameter that does not exist', async () =>  {
+      const response = await fetchCharacter('parametro qualuqer');
+      //  Verifique o que a API retorna e para isso vamos utiilize o console.log()
+      // console.log(response);
+      expect(response).toBe('Invalid id');
+    });
+
+    it('Check if fetch is called with the correct endpoint', async () => {
+      const url = 'https://www.superheroapi.com/api.php/4192484924171229/720';
+      // É necessário executar a função fetchCharacter
+      await fetchCharacter('720');
+      // É necessário esperar que a função fetch seja chamada 4 vezes
+      expect(fetch).toHaveBeenCalledTimes(4);
+      // Se a função fetch é executada com o parâmetro certo
+      expect(fetch).toHaveBeenCalledWith(url);
+    });
 });
